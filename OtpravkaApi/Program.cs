88515afd6df962c960c66337aee59a9a65d53d16
danilkurmak <dkurmak58@gmail.com>
2025-12-16 18:58:20 +0300

    update Program.cs

--- OtpravkaApi/Program.cs
+++ OtpravkaApi/Program.cs
@@ -6,20 +6,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger можно оставить и в проде (для лабы удобно)
 app.UseSwagger();
 app.UseSwaggerUI();
-
-// HTTPS редирект только локально (в контейнере чаще мешает)
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
-}
 
 app.UseAuthorization();
 app.MapControllers();
 
-// чтобы по корню домена не была "пустота"
+// чтобы корень не был пустым
 app.MapGet("/", () => "OtpravkaApi is running");
 
 app.Run();
