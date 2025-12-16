300f2a40a417b53c0ff23825b2567fa82837cb6e
danilkurmak <dkurmak58@gmail.com>
2025-12-16 17:11:51 +0300

    update amvera.yml

--- OtpravkaApi/Program.cs
+++ OtpravkaApi/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
