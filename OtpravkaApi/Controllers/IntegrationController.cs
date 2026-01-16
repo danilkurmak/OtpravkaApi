abea47fd99b77956c844c0d14ad6393ade0f84bc
danilkurmak <dkurmak58@gmail.com>
2026-01-16 18:32:48 +0300

    api

---
+++ OtpravkaApi/Controllers/IntegrationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OtpravkaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntegrationController : ControllerBase
+    {
+    }
+}
