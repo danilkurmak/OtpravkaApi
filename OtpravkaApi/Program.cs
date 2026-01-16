408ac74a2461054dd3e210ad0ba84775aa56ac5b
danilkurmak <dkurmak58@gmail.com>
2026-01-16 21:46:09 +0300

    cc

--- OtpravkaApi/Program.cs
+++ OtpravkaApi/Program.cs
@@ -4,17 +4,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
 
-app.UseDefaultFiles();   
-app.UseStaticFiles();
-
-
+// Swagger включаем всегда
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
 app.MapControllers();
+
+// редирект с корня на swagger
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.Run();
