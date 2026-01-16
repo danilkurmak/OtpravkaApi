add0d5febb1ffc5440ed605454c9ed7910a93263
danilkurmak <dkurmak58@gmail.com>
2026-01-16 21:37:36 +0300

    change

--- OtpravkaApi/Program.cs
+++ OtpravkaApi/Program.cs
@@ -6,19 +6,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 1) статика (wwwroot)
-app.UseDefaultFiles();   // чтобы "/" открывал index.html
-app.UseStaticFiles();    // чтобы вообще отдавались файлы из wwwroot
 
-// Swagger
+app.UseDefaultFiles();   
+app.UseStaticFiles();
+
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.MapControllers();
-
-// если вдруг / не отдался статикой — пусть хотя бы ведёт на интерфейс
-app.MapGet("/", () => Results.Redirect("/index.html"));
 
 app.Run();
