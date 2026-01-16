da3a7cbff81454755fa3be08444494d0686970aa
danilkurmak <dkurmak58@gmail.com>
2026-01-16 22:38:04 +0300

    sa

--- OtpravkaApi/Program.cs
+++ OtpravkaApi/Program.cs
@@ -6,16 +6,21 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+app.UseDefaultFiles();   
+app.UseStaticFiles();   
+
 // Swagger включаем всегда (и локально, и на Amvera)
 app.UseSwagger();
 app.UseSwaggerUI();
-
 
 app.UseHttpsRedirection();
 
 app.MapControllers();
 
-
+//корень сайта открывает swagger
 app.MapGet("/", () => Results.Redirect("/swagger"));
+
+app.MapGet("/ui", () => Results.Redirect("/index.html"));
 
 app.Run();
